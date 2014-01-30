7ba5fced6f80450c7c3bd439bb64ed610cb38670
Oliver Salzburg <oliver.salzburg@gmail.com>
2014-01-30 15:09:09 +0100

    Initial source commit

---
+++ IEButton/IEButton.cpp
@@ -0,0 +1,11 @@
+// This is the main DLL file.
+
+#include "stdafx.h"
+
+#include "IEButton.h"
+
+namespace IEButton {
+  void ButtonInstaller::Install(HWND ieWindow) {
+
+  }
+}
---
+++ IEButton/IEButton.h
@@ -0,0 +1,12 @@
+// IEButton.h
+
+#pragma once
+
+using namespace System;
+
+namespace IEButton {
+  public ref class ButtonInstaller {
+    // TODO: Add your methods for this class here.
+    void Install(HWND ieWindow);
+  };
+}
